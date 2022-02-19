7a0a07b7f8ffee06e1d8d0f99ae51b4f0bd420bb
ANNER RONALDO ESCOBAR CRUZ <aescobarc13@miumg.edu.gt>
2022-02-18 19:21:32 -0600

    Primera Subida

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Anner
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+       int cantidad;
+       int a = 0;
+       int b = 0;
+       int c = 1;
+       int d;
+        
+
+        
+    }
+    
+}
